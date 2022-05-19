0d50c9b8af73996db37b12e838c36650b98cd31c
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-19 14:54:56 +0200

    feat(server): finish route and schema

--- packages/server/src/routes/jobApplication.route.js
+++ packages/server/src/routes/jobApplication.route.js
@@ -1,14 +1,118 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const { application } = require('express');
+const { validationResult, checkSchema } = require('express-validator');
 const JobApplicationModel = require('../models/jobApplication.model');
+const userModel = require('../models/user.model');
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  const info = req.user;
-  console.log('INFO USER', info);
-  console.log('INFO BODY', body);
+router.post(
+  '/',
+  checkSchema({
+    ownerId: {
+      isMongoId: true,
+    },
+    state: {
+      isString: true,
+    },
+    company: {
+      isString: true,
+    },
+    titleJob: {
+      isString: true,
+    },
+    linkOffer: {
+      isURL: true,
+      optional: true,
+    },
+    address: {
+      isString: true,
+      optional: true,
+    },
+    contact: {
+      isString: true,
+      optional: true,
+    },
+    date: {
+      isDate: true,
+      toDate: true,
+    },
+    job: {
+      isString: true,
+      optional: true,
+    },
+    steps: {
+      isArray: true,
+      optional: true,
+    },
+    'steps.*': {
+      isObject: true,
+    },
+    'steps.*.stepType': {
+      isString: true,
+    },
+    'steps.*.startDate': {
+      isDate: true,
+      toDate: true,
+    },
+    'steps.*.endDate': {
+      isDate: true,
+      toDate: true,
+      optional: true,
+    },
+    'steps.*.location': {
+      isObject: true,
+      optional: true,
+    },
+    'steps.*.location.locationType': {
+      isString: true,
+      optional: true,
+    },
+    'steps.*.location.details': {
+      isString: true,
+      optional: true,
+    },
+  }),
+  (req, res) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized',
+      });
+    }
 
-  JobApplicationModel.create(body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const body = req.body;
+    const info = req.user;
+    const id = info._id;
+    const appl = '';
+
+    JobApplicationModel.create({
+      ...body,
+      ownerId: req.user._id,
+    })
+      .then((application) => {
+        userModel
+          .findOneAndUpdate(id, { $push: { jobApplications: application._id } })
+          .then((user) => {
+            console.log(user);
+            res.json(application);
+          })
+          .catch((err) => {
+            res.status(500).send(err);
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  },
+);
+
+router.get('/', (req, res) => {
+  JobApplicationModel.find()
     .then((application) => {
       res.json(application);
     })
