725ccc3476bd51e00f34be7fdabee57f9a03848e
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-23 12:09:13 +0200

    feat(client): finish navbar

--- packages/client/src/components/navbar/Navbar.jsx
+++ packages/client/src/components/navbar/Navbar.jsx
@@ -6,37 +6,49 @@ import { NavButtonIcon } from '../icons/NavButtonIcon';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
-  const [navBolean, setBolean] = useState(false);
+  const [navBolean, setNavBolean] = useState(false);
   //     let location = useLocation();
   //   React.useEffect(() => {
   //     ga.send(["pageview", location.pathname]);
   //   }, [location]);
 
   return (
-    <div className={styles.navbar}>
+    <div
+      className={`${styles.navbar} ${navBolean === true ? styles.open : ''}`}
+    >
       <div className={styles.navButton}>
+        <h2 className={`${navBolean === false ? styles.none : ''}`}>
+          SUIVI JOB
+        </h2>
         <button
           className={styles.button}
           onClick={() => {
-            setBolean(!navBolean);
+            setNavBolean(!navBolean);
           }}
         >
-          <NavButtonIcon className={styles.navButtonIcon} />
+          <NavButtonIcon
+            className={`${styles.navButtonIcon} ${
+              navBolean === true ? styles.inversed : ''
+            }`}
+          />
         </button>
       </div>
       <div className={styles.section}>
         <a>
           <HomeIcon className={styles.icon} />
+          <p>Page d'acceuil</p>
         </a>
       </div>
       <div className={styles.section}>
         <a>
           <DashboardIcon className={styles.icon} />
+          <p>Tableau de bort</p>
         </a>
       </div>
       <div className={`${styles.section} ${styles.params}`}>
         <a>
           <ParamsIcon className={styles.icon} />
+          <p>Paramètre</p>
         </a>
       </div>
     </div>
