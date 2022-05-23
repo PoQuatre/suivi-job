52917b3fe03daf748d3d6880b6746fcab5e366d5
Kar1004 <97450951+Kar1004@users.noreply.github.com>
2022-05-23 11:28:12 +0200

    feat(client): link job application to the backend

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import JobApplicationList from './components/jobApplicationList/jobApplicationList';
+
+import JobApplication from './pages/jobApplication/JobApplication';
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/list" element={<JobApplicationList />} />
+      <Route path="/job-application" element={<JobApplication />} />
     </Routes>
   );
 };
--- packages/client/src/components/jobApplicationList/JobApplicationList.jsx
+++ packages/client/src/components/jobApplicationList/JobApplicationList.jsx
@@ -2,38 +2,39 @@ import React, { useState } from 'react';
 import styles from './jobApplicationList.module.css';
 import JobApplicationListEntry from '../jobApplicationListEntry/JobApplicationListEntry';
 
-function JobApplicationList() {
-  const [lists, setLists] = useState([
-    {
-      id: '628784ffa75e69653cee7b0b',
-      enterprise: 'Lorem ipsum dolor sit amet.',
-      titleJob:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sunt, expedita quos assumenda quia ipsum.',
-      date: '20/05/2022',
-      state: 'waiting',
-    },
-    {
-      id: '628785596b7f8c77aafeac35',
-      enterprise: 'Dolor sit amet.',
-      titleJob: 'Lorem ipsum dolor sit amet consectetur assumenda quia ipsum.',
-      date: '01/05/2022',
-      state: 'denied',
-    },
-    {
-      id: '62878560a0a27ca48618eded',
-      enterprise: 'Lorem ipsum dolor.',
-      titleJob: 'Lorem quos assumenda quia ipsum.',
-      date: '21/05/2022',
-      state: 'accepted',
-    },
-    {
-      id: '62878567383cf66f086208f3',
-      enterprise: 'Lorem ipsum.',
-      titleJob: 'Lorem sunt, expedita quos assumenda quia ipsum.',
-      date: '17/05/2022',
-      state: 'no-response',
-    },
-  ]);
+function JobApplicationList(props) {
+  // const [lists, setLists] = useState([
+  //   {
+  //     id: '628784ffa75e69653cee7b0b',
+  //     enterprise: 'Lorem ipsum dolor sit amet.',
+  //     titleJob:
+  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sunt, expedita quos assumenda quia ipsum.',
+  //     date: '20/05/2022',
+  //     state: 'waiting',
+  //   },
+  //   {
+  //     id: '628785596b7f8c77aafeac35',
+  //     enterprise: 'Dolor sit amet.',
+  //     titleJob: 'Lorem ipsum dolor sit amet consectetur assumenda quia ipsum.',
+  //     date: '01/05/2022',
+  //     state: 'denied',
+  //   },
+  //   {
+  //     id: '62878560a0a27ca48618eded',
+  //     enterprise: 'Lorem ipsum dolor.',
+  //     titleJob: 'Lorem quos assumenda quia ipsum.',
+  //     date: '21/05/2022',
+  //     state: 'accepted',
+  //   },
+  //   {
+  //     id: '62878567383cf66f086208f3',
+  //     enterprise: 'Lorem ipsum.',
+  //     titleJob: 'Lorem sunt, expedita quos assumenda quia ipsum.',
+  //     date: '17/05/2022',
+  //     state: 'no-response',
+  //   },
+
+  // ]);
 
   const deleteTodo = (id) => {
     setLists((lists) => lists.filter((item) => item.id !== id));
@@ -41,23 +42,23 @@ function JobApplicationList() {
 
   return (
     <div className="list">
-      <table style={{ 'overflow-x': 'auto' }}>
+      <table>
         <thead>
           <tr></tr>
           <tr></tr>
           <tr></tr>
         </thead>
         <tbody>
-          {lists.map((list) => {
+          {props.list.map((item) => {
             return (
               <>
                 <JobApplicationListEntry
-                  id={list.id}
-                  state={list.state}
-                  enterprise={list.enterprise}
-                  titleJob={list.titleJob}
-                  date={list.date}
-                  onDelete={() => deleteTodo(list.id)}
+                  id={item.id}
+                  state={item.state}
+                  enterprise={item.company}
+                  titleJob={item.titleJob}
+                  date={item.date}
+                  onDelete={() => deleteTodo(item.id)}
                 />
               </>
             );
