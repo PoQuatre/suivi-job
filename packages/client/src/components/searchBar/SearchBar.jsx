25fd7feb2cb71cde87393fc9626774ac96eb93e9
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-24 15:42:03 +0200

    feat(client): adding searchBar

---
+++ packages/client/src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SearchIcon } from '../icons/SearchIcon';
+
+export const SearchBar = () => {
+  return (
+    <div>
+      <input>
+        <SearchIcon />
+      </input>
+    </div>
+  );
+};
