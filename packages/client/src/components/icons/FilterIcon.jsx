a9ed9ac4ece6f5fb6c6fbe09c0519c04b75cdd74
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-25 10:37:08 +0200

    feat(client): create search bar

---
+++ packages/client/src/components/icons/FilterIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const FilterIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="15"
+      viewBox="0 0 23 15"
+      fill="none"
+    >
+      <path
+        d="M4.625 6.125H18.375V8.875H4.625V6.125ZM0.5 0.625H22.5V3.375H0.5V0.625ZM8.75 11.625H14.25V14.375H8.75V11.625Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
--- packages/client/src/components/searchBar/SearchBar.jsx
+++ packages/client/src/components/searchBar/SearchBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { FilterIcon } from '../icons/FilterIcon';
 import { SearchIcon } from '../icons/SearchIcon';
+import styles from './SearchBar.module.css';
 
 export const SearchBar = () => {
   return (
-    <div>
-      <input>
+    <div className={styles.bar}>
+      <div className={styles.input}>
         <SearchIcon />
-      </input>
+        <input />
+      </div>
+      <button>
+        <FilterIcon />
+        <p>Filtre</p>
+      </button>
     </div>
   );
 };
