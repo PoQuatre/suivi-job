9e7251bde1e4789d32dfd2d7ce2527f2333643b9
PoQuatre <leflem.mateo2002@gmail.com>
2022-05-25 13:32:28 +0200

    feat(client): add the main application layout

--- packages/client/src/pages/jobApplication/JobApplication.jsx
+++ packages/client/src/pages/jobApplication/JobApplication.jsx
@@ -1,16 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import JobApplicationList from '../../components/jobApplicationList/JobApplicationList';
+import CreationForm from '../../components/creationForm/CreationForm';
+import { Navbar } from '../../components/navbar/Navbar';
+import styles from './jobApplication.module.css';
 
 function JobApplication() {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     fetch('/api/job-application')
       .then((res) => res.json())
       .then((res) => setData(res));
   }, []);
+
   return (
-    <div>
-      <JobApplicationList list={data} />
+    <div className={styles.mainContainer}>
+      <Navbar />
+      <div className={styles.splitContainer}>
+        <JobApplicationList list={data} />
+      </div>
+      <div className={styles.splitContainer}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className={styles.desktopOnly}>
+                <h1>Nothing to show</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/new"
+            element={
+              <div className={styles.popup}>
+                <CreationForm />
+              </div>
+            }
+          />
+          <Route
+            path="/*"
+            element={
+              <div className={styles.popup}>
+                <CreationForm />
+              </div>
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
